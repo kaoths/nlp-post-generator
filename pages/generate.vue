318b7050ab6f810b7323cca8df41d523f4a191a9
Kao Oat <tanawit139@gmail.com>
2021-05-02 16:19:04 +0700

    2nd

--- pages/generate.vue
+++ pages/generate.vue
@@ -1,10 +1,34 @@
 <template>
-$END$
+<!--  <v-container fluid>-->
+<dvi class="d-flex flex-column">
+  <v-textarea
+    solo
+    label="Input title"
+    v-model='inputText'
+  />
+  <v-btn @click='perform'>Generate</v-btn>
+  <v-textarea
+    solo
+    label='Generated text shown here'
+    v-model="generatedText"
+
+  />
+</dvi>
+
+<!--  </v-container>-->
 </template>
 
-<script>
-export default {
-name: "generate"
+<script lang='ts'>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class GeneratePage extends Vue {
+  private inputText = ''
+  private generatedText = ''
+  async perform() {
+    const { data } = await this.$axios.get('https://jsonplaceholder.typicode.com/posts/11')
+    this.generatedText = data.body
+  }
 }
 </script>
 

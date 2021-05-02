81e00832442bd7c10eef8dea92ea7bde2402ba92
Kao Oat <tanawit139@gmail.com>
2021-05-03 05:26:24 +0700

    change host

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   server: {
     port: process.env.PORT || 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
